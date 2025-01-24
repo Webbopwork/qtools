dc66ba0681716e805b520f93e533a45b170b74f3
Webbop <61779226+Webbopwork@users.noreply.github.com>
2025-01-24 15:57:43 +0100

    Add files via upload

--- flatlink/flatlink_1.sh
+++ flatlink/flatlink_1.sh
@@ -2,20 +2,15 @@
 if [[ -n $1 ]]; then
     if [[ -n $2 ]]; then
         alias ${1}="flatpak run ${2}"
-        echo ${1}="flatpak run ${2}"
     elif [[ $1 =~ "." ]]; then
         GREP_PART=$(flatpak list --app --columns name,application | grep -Fwi --colour=never "${1}"  | grep --colour=never -o $'^[^\t]*')
         if [[ -n $GREP_PART ]]; then
-            GREP_PART=${GREP_PART// /_}="flatpak run ${1}"
-            alias "${GREP_PART}"
-            echo $GREP_PART
+            alias ${GREP_PART// /_}="flatpak run ${1}"
         fi
     else
         GREP_PART=$(flatpak list --app --columns name,application | grep -Fwi --colour=never "${1}" | grep --colour=never -o $'\w*\.\w*\.\w*')
         if [[ -n $GREP_PART ]]; then
-            GREP_PART=${1// /_}="flatpak run ${GREP_PART}"
-            alias "${GREP_PART}"
-            echo $GREP_PART
+            alias ${1// /_}="flatpak run ${GREP_PART}"
         fi
     fi
 fi
