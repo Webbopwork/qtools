8ce5f6e8b273580845dd43d97d8ed08e9d84b9d2
eliseydudin <dudinelisey630@gmail.com>
2025-01-25 12:01:10 +0300

    apply shfmt (https://github.com/mvdan/sh) to all bash files

--- qtool/qtool.sh
+++ qtool/qtool.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ -n $1 ]]; then
-    CODE=$(curl -s "https://raw.githubusercontent.com/Webbopwork/qtools/refs/heads/main/${1}/${1}.sh")
-    if [[ "${CODE}" != "404: Not Found" ]]; then
-        source <(curl -s "https://raw.githubusercontent.com/Webbopwork/qtools/refs/heads/main/${1}/${1}.sh") ${@:2}
-    fi
+	CODE=$(curl -s "https://raw.githubusercontent.com/Webbopwork/qtools/refs/heads/main/${1}/${1}.sh")
+	if [[ "${CODE}" != "404: Not Found" ]]; then
+		source <(curl -s "https://raw.githubusercontent.com/Webbopwork/qtools/refs/heads/main/${1}/${1}.sh") ${@:2}
+	fi
 fi
--- qtool/setup.sh
+++ qtool/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 qtool() {
-    if [[ -n $1 ]]; then
-        CODE=$(curl -s "https://raw.githubusercontent.com/Webbopwork/qtools/refs/heads/main/${1}/${1}.sh")
-        if [[ "${CODE}" != "404: Not Found" ]]; then
-            source <(curl -s "https://raw.githubusercontent.com/Webbopwork/qtools/refs/heads/main/${1}/${1}.sh") ${@:2}
-        fi
-    fi
+	if [[ -n $1 ]]; then
+		CODE=$(curl -s "https://raw.githubusercontent.com/Webbopwork/qtools/refs/heads/main/${1}/${1}.sh")
+		if [[ "${CODE}" != "404: Not Found" ]]; then
+			source <(curl -s "https://raw.githubusercontent.com/Webbopwork/qtools/refs/heads/main/${1}/${1}.sh") ${@:2}
+		fi
+	fi
 }
 
 export -f qtool
