d5250a1c7a23d915ee60cc0786518d3c65b55750
Webbop <61779226+Webbopwork@users.noreply.github.com>
2025-07-12 22:53:20 +0200

    Not only are they a boyliker, but also a bluntliker

---
+++ boyliker-blunt/boyliker-blunt.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+echo "                ++::::::::::                                                                                                                          
+                ++++++++++::                                                                                                                          
+                mm++++++++                                                                                                                            
+              --++++mmmm++                                                                                                                            
+              mmmm++mmmm++                                                                                                                            
+              ++mm++++mmmm                                                                                                                            
+              mmmmmmmmmmmm                                                                                                                            
+              mmmmmmmmmmmm                                                                                                                            
+              MMMMMMMMMMmmMM                                ##    ..                                                                                  
+              mmmmmmmmmmmmmm                              ##      ..                          MM##                                                    
+              ++mmMMmmMMmmMM                            ##          ##                    ##..                                                        
+                MMMMMMmmmmmm                          ##              --                ##                                                            
+                MMMMMMMMMMmm                          mm              @@              ##            @@                                                
+                MM@@MM@@MMMMMM                      MM                              ##              MM                                                
+                MMMMMMMMMMMMMM                      ##              MM  ::        MM                  ..                                              
+                mmMMMMMMmmMMMM                      ::            ##  MM##        ..                  ##                                              
+                  @@MMMMMMMMMM                      ..            ##    MM      ##                    ##                                              
+                  MMMMMMMMMMMM                                    MM      MM                          ##                                              
+                  @@MM@@MM@@MM                                    @@                                  MM                                              
+                  MMMMMMMMMM                                    ####                                  MM                                              
+                  MMMMMMMM@@                                MM++                                      @@                                              
+                  MMMMMMMMMM                                    ##                                    @@                                              
+                  MMMMmm####..                      ..                                                @@                                              
+                  MMMM##::##  ##++                  ::                                                @@                                              
+                  MM##----##        ##..            ##::##--    ##  ##        ##@@##                  @@                                              
+                  MM##----##            ##++          ##      ##  @@##      ##  ######    ######MM--MM                                                
+                  mmmm::--##                MM##      ##      ::  ####          ######            ##@@                                                
+                  ++mm----@@                      ##::@@    ..--  ####          MM######          ##                                                  
+                  ::mm----..                          --####      ::  ####        ####..        ##                                                    
+                    ##----                                    ##@@                  ..  ..    ##                                                      
+                    @@..##                                        --##MM                                                                              
+                      ++##########################mmmmmmmmmm          ##::::                                                                          
+                                                        ##MM                                  @@                                                      
+                                                            ::                                  --                                                    
+                                                              ++..                      ######MM                                                      
+                                                            ##                          ##                                                            
+                                                          ++                              ##      mm######                                            
+                                                    @@    ..                                ..@@++        @@                                          
+                                                    ##    ..                                      ##        @@--                                      
+                                                    @@                                              ##        @@..                                    
+                                                                                                      ##          ##              ##                  
+                                                      ::                                                --          ##          ##                    
+                                                      MM                          ..##                  ##              ######    ##                  
+                                                                                      MM::              ##                        --                  
+                                                      MM--                                              ##                      MM                    
+                                                      ##                              ##                --                      ##                    
+                                                        ####                        MM                ##                        --                    
+                                                                                    ##                ::                      @@                      
+                                                                                    --              ##                        --                      
+                                                            ::                                      ##                      ##                        
+                                                            ##                                        --                  @@                          
+                                                            ++                      ::                ##                ##                            
+                                                              ##                    MM                @@            ::##                              
+                                                              MM                      ##            @@  ::######@@mm                                  
+                                                              ::..            ##        --mm::::::##                                                  
+                                                            ##                --                MM                                                    
+                                                          MM                    ##                                                                    
+                                                        ##                        ##              ..                                                  
+                                                        @@                  ::##  ##              ##                                                  
+                                                          ::##########mm                          ##                                                  
+                                                                                    ##            MM                                                  
+                                                                                    MM                                                                
+                                                                                    mm                                                                
+                                                                                      ##          ##                                                  
+                                                                                        ####      ..                                                  
+                                                                                                                                                      
+                                                                                                                                                      
+                                                                                                                                                      
+                                                                                                                                                      
+                                                                                                                                                      
+                                                                                                                                                      
+                                                                                                                                                      
+                                                                                                                                                      "
