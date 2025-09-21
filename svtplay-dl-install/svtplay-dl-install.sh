f5599fb05fc62dc9b5e3b2915b6e32d5a021a331
Webbop <61779226+Webbopwork@users.noreply.github.com>
2025-09-21 21:20:22 +0200

    Uploaded a quick tool to install svtplay-dl
    The tool helps you install videos from svtplay.se
    
    Here's the link to the install script.
    
    https://svtplay-dl.se/install/linux/

---
+++ svtplay-dl-install/svtplay-dl-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# download with curl
+sudo curl -L https://svtplay-dl.se/download/latest/svtplay-dl -o /usr/local/bin/svtplay-dl
+
+# make it executable
+sudo chmod a+rx /usr/local/bin/svtplay-dl
